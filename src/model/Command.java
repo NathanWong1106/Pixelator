8c2e21f7fe5ce072296319694e3dd6b604cc2bc5
Nathan Wong <56657800+NathanWong1106@users.noreply.github.com>
2021-06-11 18:37:11 -0400

    Bit of Documentation Left

--- src/model/Command.java
+++ src/model/Command.java
@@ -4,5 +4,8 @@ package model;
  * Interface for a command (passing a method like a variable)
  */
 public interface Command {
+    /**
+     * Run method carries the method(s) to be called through this command
+     */
     void run();
 }
--- src/model/Config.java
+++ src/model/Config.java
@@ -4,6 +4,10 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
+/**
+ * Class that contains global variables accessed by other classes in the pipeline and view.
+ * Provides a central/single source of truth for the entire program
+ */
 public class Config {
     public static int pixelSize;
     public static int[] pixelSizeOptions;
